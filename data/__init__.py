099070fa4ccb2abbbdf3debc94c2ea460990de7c
lxzzzzz <93296355+lxzzzzz@users.noreply.github.com>
2025-10-08 20:27:46 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,3 @@
-# @Author       : Ruopeng Gao
-# @Date         : 2022/7/6
-# @Description  : Data operators, such as data read, dataset, dataloader.
 
 """
 In this package, should include the main operators about dataset.
